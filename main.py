376d94c03136ac356d967e07781789c88020d1be
recomoonmoon <1125624537@qq.com>
2025-09-05 10:33:18 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from typing import  *
+from collections import defaultdict, deque
+from bisect import bisect_right,bisect_left
+import math
+import heapq
+from heapq import heappop, heapify, heappush, heappushpop, heapreplace
+
+
+
